ddfdf8f273e0ce8fb260bc646124410ff920e6b8
Andrey Polischuk <me@andrepolischuk.com>
2021-10-01 01:27:06 +0300

    refactor: fix trackers typings

--- src/tracker/trackNonLinearEvent.ts
+++ src/tracker/trackNonLinearEvent.ts
@@ -1,11 +1,16 @@
 import {getNonLinearTrackingEvents} from '../vastSelectors';
+import {ParsedAd, VastChain, VastEventTrackerOptions} from '../types';
 import createVastEventTracker from './helpers/createVastEventTracker';
 import pixelTracker from './helpers/pixelTracker';
 import {acceptInvitation, adCollapse, close} from './nonLinearEvents';
 
-const trackingEventSelector = (event) => (ad) => getNonLinearTrackingEvents(ad, event);
+const trackingEventSelector = (event: string) => (ad: ParsedAd) =>
+  getNonLinearTrackingEvents(ad, event);
+
 const linearTrackers = {
-  [acceptInvitation]: createVastEventTracker(trackingEventSelector(acceptInvitation)),
+  [acceptInvitation]: createVastEventTracker(
+    trackingEventSelector(acceptInvitation)
+  ),
   [adCollapse]: createVastEventTracker(trackingEventSelector(adCollapse)),
   [close]: createVastEventTracker(trackingEventSelector(close))
 };
@@ -13,17 +18,15 @@ const linearTrackers = {
 /**
  * Tracks the passed non linear event.
  *
- * @ignore
- * @param {string} event - name of the linear event we need to track. @see LinearEvents
- * @param {VastChain} vastChain - the ad VAST Chain.
- * @param {Object} options - Options Map. The allowed properties are:
- * @param {Object} [options.logger] - Optional logger instance.
- *                                    Must comply to the [Console interface](https://developer.mozilla.org/es/docs/Web/API/Console).
- *                                    Defaults to console.
- * @param {Object} [options.data] - additional data for the URL macro. See [VAST specification]{@link https://www.iab.com/guidelines/digital-video-ad-serving-template-vast-4-0/}
- * @param {tracker} [options.tracker] - optional tracker to use for the actual tracking. Defaults to the pixel tracker.
+ * @param event name of the linear event we need to track. @see LinearEvents
+ * @param vastChain the ad VAST Chain.
+ * @param options Options Map.
  */
-const trackNonLinearEvent = (event, vastChain, {data, tracker = pixelTracker, logger = console}) => {
+const trackNonLinearEvent = (
+  event: keyof typeof linearTrackers,
+  vastChain: VastChain,
+  {data, tracker = pixelTracker, logger = console}: VastEventTrackerOptions
+): void => {
   const linearTracker = linearTrackers[event];
 
   if (linearTracker) {
